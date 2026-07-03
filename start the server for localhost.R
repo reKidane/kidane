461cc3340183a70a362b8c4e19ec375fb7639a8d
Kidane <39365732+reKidane@users.noreply.github.com>
2026-07-03 17:34:06 +0300

    Fix Unicode symbols in bib layout and change author emphasis
    Replaced corrupted Unicode characters in regex patterns with correct special symbols (∗†‡§¶‖) used for author annotation in bibliography. Changed author self-highlighting from <strong> to <em> tags for semantic emphasis. Also clarified comment in localhost startup script.

--- start the server for localhost.R
+++ start the server for localhost.R
@@ -1,4 +1,4 @@
-#In the RStudio Terminal, run:
+#In the RStudio Terminal, run: to start localhost server
 cd /Users/kidane/Documents/Kidane_Personal_website
 export PATH="/opt/homebrew/opt/ruby/bin:/opt/homebrew/lib/ruby/gems/4.0.0/bin:$PATH"
 bundle exec jekyll serve
